03335109ddde17324e23bc9cbac24bfc67fbb90b
kachka16 <lirodaki345@gmail.com>
2025-06-23 14:21:05 +0300

    Update_9

--- Battleships/Assets/Scripts/FieldShip.cs
+++ Battleships/Assets/Scripts/FieldShip.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using System;
 
-public class FieldShip : MonoBehaviour, IUnit
+public class FieldShip : MonoBehaviour
 {
     public Vector3 GetPosition()
     {
